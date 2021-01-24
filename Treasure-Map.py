ffcde952d21e548e1737c0f287154854f0ffe802
zainab <zainab.mehdee63@ed.cna.nl.ca>
2021-01-24 12:04:18 -0330

    Modied code to be more user-friendly.

--- Treasure-Map.py
+++ Treasure-Map.py
@@ -1,15 +1,18 @@
-row1 = ["⬜️","️⬜️","️⬜️"]
-row2 = ["⬜️","⬜️","️⬜️"]
-row3 = ["⬜️","⬜️️","⬜️️"]
-map = [row1, row2, row3]
-print(f"{row1}\n{row2}\n{row3}")
+row_1 = ["⬜️","️⬜️","️⬜️"]
+row_2 = ["⬜️","⬜️","️⬜️"]
+row_3 = ["⬜️","⬜️️","⬜️️"]
 
-print("Example Position:Column 1, Row 2 = 12")
-position = input("Where do you want to put the treasure? ")
+map = [row_1, row_2, row_3]
+print(f"{row_1}\n{row_2}\n{row_3}")
 
-horizontal = int(position[0])
-vertical = int(position[1])
+print("Example Position: Column 1, Row 2 = 12")
+input_position = input("Where do you want to put the treasure? ")
+
+horizontal = int(input_position[0])
+vertical = int(input_position[1])
 
 map[vertical - 1][horizontal - 1] = " X"
 
-print(f" {row1}\n {row2}\n {row3}")
+print(f" {row_1}\n {row_2}\n {row_3}")
+
+print("You have successfully hidden your treasure!")

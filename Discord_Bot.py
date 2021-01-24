362346239bd2425a1282a94d9f7d6f89a87adf45
zainab <zainab.mehdee63@ed.cna.nl.ca>
2021-01-24 12:47:47 -0330

    Debug

--- Discord_Bot.py
+++ Discord_Bot.py
@@ -3,7 +3,6 @@ import os
 
 client = discord.Client()
 
-
 @client.event
 async def on_ready():
     print("The bot is logged in as {0.user}".format(client))
@@ -13,7 +12,7 @@ async def on_ready():
 async def on_message(message_trigger):
     if message_trigger.author == client.user:
         return  
-    if message_trigger.content.startswith("$Hello", "$hi", "$what's up", "$anyone", "$sup", "$hii", "$ello", "$Guys?", "$yo", ).lower:
+    if message_trigger.content.startswith("$Hello", "$hi" ).lower:
         await message_trigger.channel.send("What?")
 
 client.run(os.getenv("TOKEN"))

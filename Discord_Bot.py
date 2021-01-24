ba258bb6c7a9e0fff443c77138736b194030f782
zainab <zainab.mehdee63@ed.cna.nl.ca>
2021-01-24 12:38:10 -0330

    Added triggers to the bot

--- Discord_Bot.py
+++ Discord_Bot.py
@@ -3,9 +3,32 @@ client = discord.Client()
 
 
 @client.event
-
 async def on_ready():
     print("The bot is logged in as {0.user}".format(client))
 
 
- 
+@client.event
+async def on_message(message_trigger):
+    if message_trigger.author == client.user:
+        return  
+    if message_trigger.content.startswith("$Hello", "$hi", "$what's up", "$anyone", "$sup", "$hii", "$", "$", "$", ):
+        await message.channel.send("What?")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    print("The bot is logged in as {0.user}".format(client))
+
+
+

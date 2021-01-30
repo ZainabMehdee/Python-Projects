b4535e4c5def6367332f9933d5de83386d2f9c90
zainab <zainab.mehdee63@ed.cna.nl.ca>
2021-01-29 20:34:16 -0330

    Fix bugs

--- Birthday.py
+++ Birthday.py
@@ -2,7 +2,7 @@
 Create a program that asks user to enter their birthday and then 
 prints out the following pieces of information in separate lines:
     1. What day of the week they were born
-    2. How many days to go before their next birthday
+    2. How many days to go before their next birthday.
     3. What is their current age.
 
  """
@@ -10,14 +10,16 @@ from datetime import datetime
 from datetime import timedelta
 
 
-input_birthday = input("Enter your birthday (dd/mm/yyyy): ")
-dmy = list(map(int, input_birthday.split('/')))
+
 current_date_time = datetime.now()
 
 try:
-    birth_date_time = datetime(dmy[2], dmy[1], dmy[0])
+    input_birthday = input("Enter your birthday (dd/mm/yyyy): ")
+    split_birthday = list(map(int, input_birthday.split('/')))
+    birth_date_time = datetime(split_birthday[2], split_birthday[1], split_birthday[0])
 except:
     print("either your birthday doesn't exist or you typed it wrong.")
+    exit()
     
 day_of_the_week_born = birth_date_time.strftime('%A')
 
@@ -25,11 +27,16 @@ next_birthday = datetime(current_date_time.year, birth_date_time.month, birth_da
 
 if (current_date_time > next_birthday):
     #look at next year
-    next_birthday = datetime(birth_date_time.year + 1, birth_date_time.month, birth_date_time.day)
+    next_birthday = datetime(current_date_time.year + 1, birth_date_time.month, birth_date_time.day)
 
 days_before_next_birthday = (next_birthday - current_date_time).days
-if(current_date_time.month >= birth_date_time.month):
-    if(current_date_time.month == birth_date_time.month):
-        if(current_date_time)
+
+current_age = current_date_time.year - birth_date_time.year
+
+if current_date_time.month < birth_date_time.month:
+    current_age -= 1
+if current_date_time.month == birth_date_time.month:
+    if current_date_time.day < birth_date_time.day:
+        current_age -= 1
 
 print(day_of_the_week_born, days_before_next_birthday, current_age)
---
+++ how_much_longer.py
@@ -0,0 +1,26 @@
+#Create a function called how_much_longer() which when called, prints the number of seconds
+#left to go until the weekend (Friday 5PM). If it is currently the weekend, it should print how much
+#longer they have left before monday 7am.
+
+from datetime import datetime
+from datetime import timedelta
+
+def how_much_longer():
+    current_time = datetime.now()
+    weekend_start = current_time + timedelta(days=(6 - current_time.date().weekday() + 4))
+    weekend_start = weekend_start.replace(hour =17)
+    weekday_start = current_time + timedelta(days=(6 - current_time.date().weekday()))
+    weekday_start = weekday_start.replace(hour =7)
+
+
+    if weekend_start > weekday_start:
+        #tis weekend
+        timeleft = (weekday_start - current_time)
+        print(timeleft, timeleft.total_seconds())
+        print('until weekday')
+    else:
+        #tis weekday
+        timeleft = (weekend_start - current_time)
+        print(timeleft, timeleft.total_seconds())
+        print('until weekend')
+how_much_longer()
